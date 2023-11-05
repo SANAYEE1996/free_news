d0927d76d12d3647e3d7a4148bd91b51c5615d78
SANAYEE <dudtkd0219@gmail.com>
2023-11-06 00:43:00 +0900

    test

---
+++ src/main/java/toy/free_news/FreeNewsApplication.java
@@ -0,0 +1,13 @@
+package toy.free_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/free_news/FreeNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.free_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
